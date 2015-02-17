091c1ed0f1bfbd44b43d664229c743b0bf87e2c5
Pulkit Singhal <pulkit@shoppinpal.com>
2015-02-17 10:56:11 -0600

    issue#5 - generate-stock-order command should prompt for an appropriate interval

--- commands/generate-stock-order.js
+++ commands/generate-stock-order.js
@@ -1,4 +1,6 @@
 var Command = require('ronin').Command;
+var choose = require('asking').choose;
+var ask = require('asking').ask;
 
 var vendSdk = require('vend-nodejs-sdk')({});
 var utils = require('./../utils/utils.js');
@@ -13,55 +15,99 @@ var GenerateStockOrder = Command.extend({
 
   options: {
     token: 'string',
-    domain: 'string'
+    domain: 'string',
+    orderName: 'string',
+    outletId: 'string',
+    supplierId: 'string',
+    interval: 'string'
   },
 
-  run: function (token, domain) {
+  run: function (token, domain, orderName, outletId, supplierId) {
     var commandName = path.basename(__filename, '.js');
 
     var connectionInfo = utils.loadOauthTokens(token, domain);
+    if (!orderName) {
+      throw new Error('--orderName should be set');
+    }
+    if (!outletId) {
+      throw new Error('--outletId should be set');
+    }
+    if (!supplierId) {
+      throw new Error('--supplierId should be set');
+    }
 
-    var args = vendSdk.args.consignments.stockOrders.create();
-    args.name.value = 'blah';
-    args.outletId.value = '123';
-    //args.supplierId.value = '123';
+    var aWeekAgo = moment.utc().subtract(1, 'weeks');
+    var twoWeeksAgo = moment.utc().subtract(2, 'weeks');
+    var aMonthAgo = moment.utc().subtract(1, 'months');
+    var twoMonthsAgo = moment.utc().subtract(2, 'months');
+    var intervalOptions = [
+      aWeekAgo.format('YYYY-MM-DD'),
+      twoWeeksAgo.format('YYYY-MM-DD'),
+      aMonthAgo.format('YYYY-MM-DD'),
+      twoMonthsAgo.format('YYYY-MM-DD')
+    ];
+    var intervalOptionsForDisplay = [
+      'Starting a week ago (' + aWeekAgo.format('YYYY-MM-DD') + ')',
+      'Starting two weeks ago (' + twoWeeksAgo.format('YYYY-MM-DD') + ')',
+      'Starting a month ago (' + aMonthAgo.format('YYYY-MM-DD') + ')',
+      'Starting two months ago (' + twoMonthsAgo.format('YYYY-MM-DD') + ')'
+    ];
 
-    return vendSdk.consignments.stockOrders.create(connectionInfo)
-      .then(function(data) {
-        console.log(data);
-      })
-      /*.then(function(products) {
-        console.log(commandName + ' > 1st then block');
-        return utils.updateOauthTokens(connectionInfo,products);
-      })
-      .then(function(products) {
-        console.log(commandName + ' > 2nd then block');
-        return utils.exportToJsonFileFormat('export-all-products', products)
-          .then(function() {
-            return Promise.resolve(products);
-          });
-      })
-      .then(function(products) {
-        console.log(commandName + ' > 3rd then block');
-        //console.log(products);
-
-        console.log(commandName + ' > products.length: ', products.length);
-        //console.log('products: ', JSON.stringify(products,vendSdk.replacer,2));
-
-        return vendSdk.outlets.fetch({}, connectionInfo)
-          .then(function(outletsResponse) {
-            //console.log('outletsResponse: ', outletsResponse);
-            console.log(commandName + ' > outletsResponse.outlets.length: ', outletsResponse.outlets.length);
-            //console.log('outletsResponse.outlets: ' + JSON.stringify(outletsResponse.outlets,vendSdk.replacer,2));
-
-            utils.exportProductsToCsvFileFormat(products, outletsResponse.outlets); // TODO: promisify somehow and then return the promise
-            //return Promise.resolve(); // there is no way that this line actually works
-          });
-      })*/
-      .catch(function(e) {
-        console.error(commandName + ' > An unexpected error occurred: ', e);
+    choose('How far back from today should the sales history be analyzed?',
+      intervalOptionsForDisplay,
+      function (err, startAnalyzingSalesHistorySince, indexOfSelectedValue) {
+        if (indexOfSelectedValue === undefined) {
+          console.log('Incorrect selection! Please choose an option between 1 - ' + intervalOptions.length);
+        }
+        else {
+          console.log('startAnalyzingSalesHistorySince: ' + intervalOptions[indexOfSelectedValue]);
+          //runMe(connectionInfo, orderName, outletId, supplierId);
+        }
       });
   }
 });
+
+var runMe = function(connectionInfo, orderName, outletId, supplierId){
+  var args = vendSdk.args.consignments.stockOrders.create();
+  args.name.value = orderName;
+  args.outletId.value = outletId;
+  args.supplierId.value = supplierId;
+
+  return vendSdk.consignments.stockOrders.create(args, connectionInfo)
+    .then(function(data) {
+      console.log(data);
+    })
+    /*.then(function(products) {
+      console.log(commandName + ' > 1st then block');
+      return utils.updateOauthTokens(connectionInfo,products);
+    })
+    .then(function(products) {
+      console.log(commandName + ' > 2nd then block');
+      return utils.exportToJsonFileFormat('export-all-products', products)
+        .then(function() {
+          return Promise.resolve(products);
+        });
+    })
+    .then(function(products) {
+      console.log(commandName + ' > 3rd then block');
+      //console.log(products);
+
+      console.log(commandName + ' > products.length: ', products.length);
+      //console.log('products: ', JSON.stringify(products,vendSdk.replacer,2));
+
+      return vendSdk.outlets.fetch({}, connectionInfo)
+        .then(function(outletsResponse) {
+          //console.log('outletsResponse: ', outletsResponse);
+          console.log(commandName + ' > outletsResponse.outlets.length: ', outletsResponse.outlets.length);
+          //console.log('outletsResponse.outlets: ' + JSON.stringify(outletsResponse.outlets,vendSdk.replacer,2));
+
+          utils.exportProductsToCsvFileFormat(products, outletsResponse.outlets); // TODO: promisify somehow and then return the promise
+          //return Promise.resolve(); // there is no way that this line actually works
+        });
+    })*/
+    .catch(function(e) {
+      console.error(commandName + ' > An unexpected error occurred: ', e);
+    });
+};
 
 module.exports = GenerateStockOrder;
