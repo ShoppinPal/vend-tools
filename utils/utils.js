5d0b7c1b051ebefb6e38370bb4116dc3066decbc
Pulkit Singhal <pulkit@shoppinpal.com>
2015-03-02 10:55:12 -0600

    remove tags column from product export
    ... The "Tags" are messing up other retailer software down the pipeline which consumes the CSV file ... due to the tag values being separated by commas

--- utils/utils.js
+++ utils/utils.js
@@ -247,7 +247,7 @@ var exportProductsToCsvFileFormat = function(products, outlets){
     'variant_option_one_name','variant_option_one_value',
     'variant_option_two_name','variant_option_two_value',
     'variant_option_three_name','variant_option_three_value',
-    'tags','supply_price','retail_price',
+    /*'tags',*/'supply_price','retail_price',
     'account_code','account_code_purchase','brand_name',
     'supplier_name','supplier_code','active','track_inventory'];
   _.each(outlets, function(outlet){
@@ -266,7 +266,7 @@ var exportProductsToCsvFileFormat = function(products, outlets){
         'variant_option_one_name','variant_option_one_value',
         'variant_option_two_name','variant_option_two_value',
         'variant_option_three_name','variant_option_three_value',
-        'tags','supply_price','retail_price',
+        /*'tags',*/'supply_price','retail_price',
         'account_code','account_code_purchase','brand_name',
         'supplier_name','supplier_code','active','track_inventory');
       neoProduct['retail_price'] = product['price'];
