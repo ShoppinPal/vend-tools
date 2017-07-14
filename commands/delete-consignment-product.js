d12c17dc72b6bcaa090ec6a532004d6ba60fc3e3
Yash Gandhi <ysg410@gmail.com>
2017-07-15 00:54:13 +0530

    tool to delete consignment products with receive qty zero

---
+++ commands/delete-consignment-product.js
@@ -0,0 +1,140 @@
+var Command = require('ronin').Command;
+
+var vendSdk = require('vend-nodejs-sdk')({});
+var utils = require('./../utils/utils.js');
+var fileSystem = require('q-io/fs');
+var Promise = require('bluebird');
+var moment = require('moment');
+var _ = require('underscore');
+var path = require('path');
+
+var DeleteConsignmentProduct = Command.extend({
+  desc: 'Delete consignment products with receive qty 0',
+
+  options: {
+    //id: 'string'
+  },
+
+  run: function () {
+    var commandName = path.basename(__filename, '.js');
+    var token = this.global.token;
+    var domain = this.global.domain;
+
+    
+    var connectionInfo = utils.loadOauthTokens(token, domain);
+    //var consignments = require('../downloadedConsignments.json');
+    
+    return vendSdk.consignments.stockOrders.fetchAll(connectionInfo)
+      .tap(function(response) {
+        
+        return utils.updateOauthTokens(connectionInfo);
+      })
+      .then(function(consignments) {
+  
+        var receiveQtyZeroConsignmentProducts = [];
+        
+        if (consignments) {
+        
+          var filteredConsignments = _.where(consignments,{"status":"SENT","type":"SUPPLIER"});
+          
+
+          return Promise.map(
+            filteredConsignments,
+            function(singleConsignment){
+
+              var receiveQtyZeroConsignmentProductsCount = 0;
+              var deletedConsignmentProductsCount = 0;
+              var argsForConsignmentProduct = {
+                consignmentId : {required: true,key: 'consignment_id', value : singleConsignment.id}
+              };
+              
+              return vendSdk.consignments.products.fetchAllByConsignment(argsForConsignmentProduct,connectionInfo)
+              .then(function(consignmentProducts) {
+                
+                consignmentProducts.forEach(function(singleConsignmentProduct){
+                  if(parseFloat(singleConsignmentProduct.count) > 0 && singleConsignmentProduct.received == undefined){
+                    receiveQtyZeroConsignmentProductsCount += 1;  
+                    receiveQtyZeroConsignmentProducts.push({'consignmentProduct':singleConsignmentProduct,'consignmentProductScriptState':'identified'});
+                  }
+                });
+                
+                return Promise.map(
+                  receiveQtyZeroConsignmentProducts,
+                  function(singleConsignmentProductToDelete){
+                    var argsToDeleteConsignmentProduct = {apiId:{value : singleConsignmentProductToDelete.consignmentProduct.id}};
+                    return vendSdk.consignments.products.remove(argsToDeleteConsignmentProduct,connectionInfo)
+                      .then(function deletionResult(result){
+                        
+                        if(result.status=="success"){
+                          deletedConsignmentProductsCount += 1;
+                          singleConsignmentProductToDelete.consignmentProductScriptState = 'Deleted';
+                        }
+                        else{
+                          singleConsignmentProductToDelete.consignmentProductScriptState = 'NotDeleted';
+                        }
+                        return Promise.resolve();
+                      })
+                      .catch(function(error){
+                        console.log(commandName + 'Error while deleting consignment product.\n'+error);
+                        return Promise.reject(commandName + 'Error while deleting consignment product.\n'+error);
+                      })
+                  },
+                  {concurrency:1}
+                  )
+                .then(function(){
+
+                  if(receiveQtyZeroConsignmentProductsCount == deletedConsignmentProductsCount){
+
+                    var argsForMarkingReceived = {apiId:{value : singleConsignment.id},body:{value:singleConsignment}};
+
+                    return vendSdk.consignments.stockOrders.markAsReceived(argsForMarkingReceived,connectionInfo)
+                      .then(function(markingResult){
+                        singleConsignmentProductToDelete.consignmentProductScriptState = 'markAsReceived';
+                        return Promise.resolve();
+                      })
+                      .catch(function(error){
+                        console.log(commandName + 'Error while marking consignment as received.\n'+error);
+                        return Promise.reject(commandName + 'Error while marking consignment as received.\n'+error);
+                      });
+                  }
+                  else{
+                    console.log('All receive qty zero consignment products not deleted.');
+                    return Promise.reject('All receive qty zero consignment products not deleted.');
+                  }
+                   
+                })
+                .catch(function(error){
+                  console.log(commandName + ' > An unexpected error occurred: ', error);
+                  return Promise.reject(commandName + ' > An unexpected error occurred: ', error);
+                }) 
+                  }
+              })
+              .catch(function(error){
+                console.log(commandName + 'Error while fetching consignment products.\n'+error);
+                return Promise.reject(commandName + 'Error while fetching consignment products.\n'+error);
+              });
+
+            },
+            {concurrency:1}
+            )
+            .then(function(){
+              console.log("Done processing.");
+              return utils.exportToJsonFileFormat(commandName,receiveQtyZeroConsignmentProducts);
+            })
+            .catch(function(error){
+              console.log(commandName + ' > An unexpected error occurred: ', error);
+              return Promise.reject(commandName + ' > An unexpected error occurred: ', error);
+            }) 
+        }
+        else {
+          console.log(commandName + ' > No matching result(s) were found.');
+          return Promise.resolve();
+        }
+      })
+      .catch(function(e) {
+        console.error(commandName + ' > An unexpected error occurred: ', e);
+      });
+  }
+});
+
+module.exports = DeleteConsignmentProduct;
